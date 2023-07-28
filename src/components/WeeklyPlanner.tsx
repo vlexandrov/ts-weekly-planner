da78ae063d5aedef90f26adbd6b076ec1bd6235b
Vlex Ponsaran <vlexponsaran@Vlexs-MacBook-Air.local>
2023-07-29 04:17:53 +1200

    removed day buttons

--- src/components/WeeklyPlanner.tsx
+++ src/components/WeeklyPlanner.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { format, addDays } from 'date-fns';
-import Day from './Day';
 import './WeeklyPlanner.css'; // Import the CSS file
 
 interface Task {
@@ -12,6 +11,7 @@ interface Task {
 const WeeklyPlanner: React.FC = () => {
   const today = new Date();
   const days = Array.from({ length: 7 }, (_, i) => addDays(today, i));
+  const dayOptions = days.map((day) => format(day, 'EEEE'));
 
   const [tasks, setTasks] = useState<Task[]>([]);
   const [taskDescription, setTaskDescription] = useState('');
@@ -22,8 +22,8 @@ const WeeklyPlanner: React.FC = () => {
     setTaskDescription(event.target.value);
   };
 
-  const handleDayClick = (day: string) => {
-    setSelectedDay(day);
+  const handleDayChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedDay(event.target.value);
   };
 
   const handleAddTask = () => {
@@ -45,20 +45,17 @@ const WeeklyPlanner: React.FC = () => {
   return (
     <div className="weekly-planner">
       <h2>Weekly Planner</h2>
-      <div className="days-container">
-        {days.map((day) => (
-          <Day
-            key={format(day, 'yyyy-MM-dd')}
-            day={format(day, 'EEEE')}
-            selected={format(day, 'EEEE') === selectedDay}
-            onClick={() => handleDayClick(format(day, 'EEEE'))}
-          />
-        ))}
-      </div>
       {showForm ? (
         <div>
           <h3>Create Task:</h3>
           <input type="text" value={taskDescription} onChange={handleTaskDescriptionChange} />
+          <select value={selectedDay} onChange={handleDayChange}>
+            {dayOptions.map((dayOption) => (
+              <option key={dayOption} value={dayOption}>
+                {dayOption}
+              </option>
+            ))}
+          </select>
           <button className="add-task-button" onClick={handleAddTask}>
             Add Task
           </button>
@@ -73,22 +70,20 @@ const WeeklyPlanner: React.FC = () => {
       )}
       <div className="tasks-section">
         <h3>Tasks:</h3>
-        <div className="day-tasks">
-            {days.map((day) => {
-            const tasksForDay = tasks.filter((task) => task.day === format(day, 'EEEE'));
+        {days.map((day) => {
+          const tasksForDay = tasks.filter((task) => task.day === format(day, 'EEEE'));
 
-            return (
-                <div key={format(day, 'yyyy-MM-dd')}>
-                <h4>{format(day, 'EEEE')}</h4>
-                <ul>
-                    {tasksForDay.map((task) => (
-                    <li key={task.id}>{task.description}</li>
-                    ))}
-                </ul>
-                </div>
-            );
-            })}
-        </div>
+          return (
+            <div key={format(day, 'yyyy-MM-dd')}>
+              <h4>{format(day, 'EEEE')}</h4>
+              <ul>
+                {tasksForDay.map((task) => (
+                  <li key={task.id}>{task.description}</li>
+                ))}
+              </ul>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
